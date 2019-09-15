1fe76d697cb55220ec5d298b07fdf02cf2066c3a
skaneko-0311 <skaneko.0311@gmail.com>
2019-09-15 21:37:55 +0900

    Call API

--- myscript.js
+++ myscript.js
@@ -1,8 +1,20 @@
 var firstFlg = true;
+const API_URL = 'https://reqres.in/api/users?page=2'
+
+var callAPI = function (){
+    $.ajax({
+        dataType: "json",
+        url: API_URL,
+        success: function (res) {
+            console.log(res);
+        }
+      });
+}
 
 var getSelectedText = function() {
     let text = window.getSelection().toString();
     if(firstFlg && text){
+        callAPI();
         console.log(text);
         firstFlg = false;
     } else if(!firstFlg && !text){
