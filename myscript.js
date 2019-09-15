1e06914595d6f0547ad60a3854abd97780e72657
skaneko-0311 <skaneko.0311@gmail.com>
2019-09-15 22:21:03 +0900

    Call AWS Translate API

--- myscript.js
+++ myscript.js
@@ -1,20 +1,38 @@
 var firstFlg = true;
-const API_URL = 'https://reqres.in/api/users?page=2'
+const awsAccessKey = 'xxx'
+const awsSecretAccessKey = 'yyy'
+AWS.config.credentials = new AWS.Credentials(awsAccessKey, awsSecretAccessKey);
+const Translate = new AWS.Translate({region: 'zzz'});
 
-var callAPI = function (){
-    $.ajax({
-        dataType: "json",
-        url: API_URL,
-        success: function (res) {
-            console.log(res);
+function translate(text) {
+    return new Promise(((resolve, reject) => {
+        let params = {
+            Text: text,
+            SourceLanguageCode: 'en',
+            TargetLanguageCode: 'ja',
         }
-      });
+        Translate.translateText(params, function(err,data){
+            if (err) {
+                console.log(err);
+                reject();
+            } else {
+                console.log(JSON.stringify(data));
+                resolve(data);
+            }
+        });
+    }));
+};
+
+var callAPI = function (text){
+    translate(text).then(function (result) {
+            console.log(result);
+    });
 }
 
 var getSelectedText = function() {
     let text = window.getSelection().toString();
     if(firstFlg && text){
-        callAPI();
+        callAPI(text);
         console.log(text);
         firstFlg = false;
     } else if(!firstFlg && !text){
